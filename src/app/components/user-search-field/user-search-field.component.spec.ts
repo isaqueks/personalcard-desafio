c8e5490dd401c4e16208610591bcd19be8d94160
isaqueks <iskschluter@gmail.com>
2023-08-17 17:02:00 -0300

    add more unit tests

--- src/app/components/user-search-field/user-search-field.component.spec.ts
+++ src/app/components/user-search-field/user-search-field.component.spec.ts
@@ -1,21 +1,77 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserSearchFieldComponent } from './user-search-field.component';
+import { MockComponent } from 'ng-mocks';
+import { FieldComponent } from '../field/field.component';
+import { UserService } from 'src/app/services/user.service';
+import { of } from 'rxjs';
 
 describe('UserSearchFieldComponent', () => {
-  let component: UserSearchFieldComponent;
-  let fixture: ComponentFixture<UserSearchFieldComponent>;
+    let component: UserSearchFieldComponent;
+    let fixture: ComponentFixture<UserSearchFieldComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [UserSearchFieldComponent]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [
+                UserSearchFieldComponent,
+                MockComponent(FieldComponent)
+            ],
+            providers: [
+                {
+                    provide: UserService,
+                    useValue: {
+                        fetch: (...args: any) => of({data:[
+                            { name: 'A', id: 5 },
+                            { name: 'B', id: 6 }
+                        ]})
+                    }
+                }
+            ]
+        });
+        fixture = TestBed.createComponent(UserSearchFieldComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
     });
-    fixture = TestBed.createComponent(UserSearchFieldComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should set currentValue to user name', () => {
+        component.user = { name: 'C', id: 5 } as any;
+        component.ngOnChanges({ user: true } as any);
+        expect(component.currentValue).toBe('C');
+    });
+
+    it('should load suggestions when value changes', () => {
+        component.setCurrentValue('jjj');
+        window.clearTimeout(component['timeoutHandler']);
+        component.loadSuggestions();
+        expect(component.suggestedUsers).toEqual([
+            { name: 'A', id: 5 },
+            { name: 'B', id: 6 }
+        ] as any);
+
+        const subs = component.userChange.subscribe((user) => {
+            expect(user).toBeUndefined(); // No user named "jjj"
+        });
+        component.checkSelected();
+        subs.unsubscribe();
+    })
+
+    it('should select user matching name', () => {
+        component.setCurrentValue('A');
+        window.clearTimeout(component['timeoutHandler']);
+        component.loadSuggestions();
+
+        const subs = component.userChange.subscribe((user) => {
+            expect(user).toEqual({
+                name: 'A',
+                id: 5
+            } as any); // No user named "jjj"
+        });
+        
+        component.checkSelected();
+        subs.unsubscribe();
+    })
 });
--- src/app/components/user-single-view/user-single-view.component.spec.ts
+++ src/app/components/user-single-view/user-single-view.component.spec.ts
@@ -1,21 +1,52 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserSingleViewComponent } from './user-single-view.component';
+import { MockComponent } from 'ng-mocks';
+import { SkeletonPlaceholderComponent } from '../skeleton-placeholder/skeleton-placeholder.component';
+import { UserViewComponent } from '../user-view/user-view.component';
+import { UserService } from 'src/app/services/user.service';
+import { of } from 'rxjs';
 
 describe('UserSingleViewComponent', () => {
-  let component: UserSingleViewComponent;
-  let fixture: ComponentFixture<UserSingleViewComponent>;
+    let component: UserSingleViewComponent;
+    let fixture: ComponentFixture<UserSingleViewComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [UserSingleViewComponent]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [
+                UserSingleViewComponent,
+                MockComponent(SkeletonPlaceholderComponent),
+                MockComponent(UserViewComponent)
+            ],
+            providers: [
+                {
+                    provide: UserService,
+                    useValue: {
+                        fetchById: (id: number) => of({
+                            id,
+                            name: 'mocked name',
+                            email: 'mocked email',
+                        })
+                    }
+                }
+            ]
+        });
+        fixture = TestBed.createComponent(UserSingleViewComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
     });
-    fixture = TestBed.createComponent(UserSingleViewComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should load the user by id', () => {
+        component.userId = 111;
+        component.ngOnInit();
+        expect(component.user).toEqual({
+            id: 111,
+            name: 'mocked name',
+            email: 'mocked email',
+        } as any);
+    })
 });
